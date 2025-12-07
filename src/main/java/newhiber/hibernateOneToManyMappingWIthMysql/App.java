179353965e1d6ab74cc0a90496c74a43e41813bb
Jayanta66 <jayhalder18@gmail.com>
2025-12-07 10:13:12 +0530

    hibernate One To Many Mapping with Mysql

--- src/main/java/newhiber/hibernateOneToManyMappingWIthMysql/App.java
+++ src/main/java/newhiber/hibernateOneToManyMappingWIthMysql/App.java
@@ -23,52 +23,66 @@ public class App
 		cfg.configure("hibernate.cfg.xml");
 		SessionFactory factory = cfg.buildSessionFactory();
 		
-
-		Question q1 = new Question();
-		q1.setQuestionId(1212);
-		q1.setQuestion("What is java .");
+/// To Save into database
 		
-		Answer answer= new Answer();
-		answer.setAnswer_id(243);
-		answer.setAnswer("Java is programming language ");
-		answer.setQuestion(q1);
-
-
-		Answer answer1= new Answer();
-		answer1.setAnswer_id(246);
-		answer1.setAnswer("Java is Oops ");
-		answer1.setQuestion(q1);
-
-
-		Answer answer2= new Answer();
-		answer2.setAnswer_id(245);
-		answer2.setAnswer("Java is Oops ");
-		answer2.setQuestion(q1);
-
-		List<Answer> list = new ArrayList<Answer>();
-		list.add(answer);
-		list.add(answer1);
-		list.add(answer2);
 		
-		q1.setAnswer(list);
-		
+//		Question q1 = new Question();
+//		q1.setQuestionId(1212);
+//		q1.setQuestion("What is java .");
+//		
+//		Answer answer= new Answer();
+//		answer.setAnswer_id(243);
+//		answer.setAnswer("Java is programming language ");
+//		answer.setQuestion(q1);
+//
+//
+//		Answer answer1= new Answer();
+//		answer1.setAnswer_id(246);
+//		answer1.setAnswer("Java is Oops ");
+//		answer1.setQuestion(q1);
+//
+//
+//		Answer answer2= new Answer();
+//		answer2.setAnswer_id(245);
+//		answer2.setAnswer("Java is Oops ");
+//		answer2.setQuestion(q1);
+//
+//		List<Answer> list = new ArrayList<Answer>();
+//		list.add(answer);
+//		list.add(answer1);
+//		list.add(answer2);
+//		
+//		q1.setAnswer(list);
+//		
+	
+//	
+//		s.persist(q1);
+//		s.persist(answer);
+//		s.persist(answer1);
+//		s.persist(answer2);
+//
+//		
+
+
 		Session s = factory.openSession();
 		Transaction tx = s.beginTransaction();
-		
 	
-		s.persist(q1);
-		s.persist(answer);
-		s.persist(answer1);
-		s.persist(answer2);
+		
+		//Fetching from Database
 
+		Question q = (Question)s.get(Question.class, 1212);
+		
+		System.out.println(q.getQuestion());
+		
+		for(Answer a:q.getAnswer()) {
+			
+			System.out.println(a.getAnswer());
+
+		}
 		
 
 		tx.commit();
 
-		
-//		Question newQ = s.get(Question.class, 1212);
-//		System.out.println(newQ.getQuestion());
-//		
 		s.close();
 		
 		
